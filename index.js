ae99a7fc4c49794a9f5f4b6c18a71590b629af45
unknown <ummatimuhammadiyy@gmail.com>
2022-02-09 01:10:42 +0500

    vfv

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-let math = require("mathjs")
 
 const exludeRandNum = ( max, exclude = []) => {
-    let num = Math.floor(Math.random() * (max - 1 + 1 - exclude.length) + 1);
-    exclude
-      .slice()
-      .sort((a, b) => a - b)
-      .every((exeption) => exeption <= num && (num++, true));
-    return num;
-  };
+  let num = Math.floor(Math.random() * (max - 1 + 1 - exclude.length) + 1);
+  exclude
+    .slice()
+    .sort((a, b) => a - b)
+    .every((exeption) => exeption <= num && (num++, true));
+  return num;
+};
 
 function GenerateMatrix() {
     let arr = [];
 
     for(let i = 0; i < 5; i++) {
-        let rn = exludeRandNum(6, [])
-        let rn1 = exludeRandNum(6, [])
-        let rn2 = exludeRandNum(6, [])
-        let rn3 = exludeRandNum(6, [])
+        let rn = exludeRandNum(5, [])
+        let rn1 = exludeRandNum(5, [])
+        let rn2 = exludeRandNum(5, [])
+        let rn3 = exludeRandNum(5, [])
         arr.push([rn,rn1,rn2,rn3])
     }
-    return math.matrix(arr)._data;
+    return (arr);
 }
 
 console.log(GenerateMatrix());
